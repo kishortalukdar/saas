be142ab47036cc8f2702b0e1febbfc1ec48ad6e8
unknown <kishortalukdar55@gmail.com>
2025-06-29 15:56:11 +0530

    project setup

--- apps/http-backend-inv/src/index.ts
+++ apps/http-backend-inv/src/index.ts
@@ -18,5 +18,9 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 
+app.listen(5001,()=>{
+    console.log("server is running ");
+})
+
 
   
---
+++ apps/http-backend-inv/src/user/routes/tenant.routes.ts
@@ -0,0 +1,9 @@
+
+import { Router } from "express";
+
+const router = Router();
+
+router.route("/send").post();
+
+
+export default router;
